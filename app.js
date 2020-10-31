e005ae3aa2504261ace52832e175a74396790fe7
Rottzll <ordenador.0930@gmail.com>
2020-10-31 17:29:22 +0900

    템플릿 상속 및 미들웨어, bodyparser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const admin = require('./routes/admin');
 const contacts = require('./routes/contacts');
@@ -12,17 +14,33 @@ nunjucks.configure('template', {
     express : app
 });
 
+//미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extends : false}));
+app.use((req,res,next) => {
+    req.body={
+
+    }
+});
+
+
 app.get('/', (req, res) =>{
     res.send('hello express');
 });
 
-app.use('/admin', admin);
+app.use('/admin', vipMW ,admin);
 
 app.use('/contacts', contacts);
 
 app.get('/testing', (req, res) =>{
     res.send('완료');
 });
+
+function vipMW(req, res, next){
+    console.log('최우선 미들웨어');
+    next();
+}
 
 app.listen( port , ()=>{
     console.log('Express listening on port', port);
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) =>{
+function T_MiddleWare(req, res, next){
+    console.log('첫번째 미들웨어');
+    next();
+}
+
+function T_MiddleWare2(req, res, next){
+    console.log('2번째 미들웨어');
+    next();
+}
+
+function loginReq(req,res,next){
+
+}
+
+router.get('/', T_MiddleWare, T_MiddleWare2, (req, res) =>{
+
     res.send('admin 이후 url');
 });
 
@@ -11,6 +26,16 @@ router.get('/products', (req, res) =>{
         message : 'hello?',
         online : 'express'
     })
+});
+
+router.get('/products/write', (req, res) => {
+
+        res.render('admin/write.html')
+});
+
+router.post('/products/write', (req,res)=>{
+
+        res.send(req.body)
 });
 
 module.exports = router;
