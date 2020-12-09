d784d31d4e45b0298a59915ff5dfa7d9f09e5d65
Rottzll <ordenador.0930@gmail.com>
2020-12-09 15:31:36 +0900

    DB 수정&삭제

--- controller/admin/admin.ctrl.js
+++ controller/admin/admin.ctrl.js
@@ -14,7 +14,7 @@ exports.get_products = (_, res) => {
 
 exports.get_products_write = (_, res) => {
     res.render('admin/write.html');
-}
+};
 
 exports.post_products_write = (req, res) => {
     //res.send(req.body);
@@ -27,7 +27,32 @@ exports.get_products_detail = (req,res) => {
     models.products.findByPk(req.params.id).then((ProductsList) =>{
         res.render('admin/detail.html', {ProductsList});
     });
+};
 
+exports.get_products_edit = (req, res) => {
+    models.products.findByPk(req.params.id).then( (ProductsList) => {
+        res.render('admin/write.html', {ProductsList});
+    });
+};
+
+exports.post_products_edit = (req, res) => {
+    models.products.update({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    },{
+        where : {id : req.params.id}   
+    }).then (() => {
+        res.redirect('/admin/products/detail/' + req.params.id);
+    });
 }
+
+exports.get_products_delete = (req,res) => {
+    models.products.destroy({
+        where : {id : req.params.id}
+    }).then(()=>{
+        res.redirect('/admin/products');
+    });
+};
 
 //라우팅 - 컨트롤러 역할
--- controller/admin/index.js
+++ controller/admin/index.js
@@ -10,7 +10,10 @@ function testMiddleWare(req, res, next){
 router.get('/products', ctrl.get_products);
 router.get('/products/write', ctrl.get_products_write);
 router.post('/products/write', ctrl.post_products_write);
-router.get('/product/detail/:id', ctrl.get_products_detail);
+router.get('/products/detail/:id', ctrl.get_products_detail);
+router.get('/products/edit/:id', ctrl.get_products_edit);
+router.post('/products/edit/:id', ctrl.post_products_edit);
+router.get('/products/delete/:id', ctrl.get_products_delete);
 
 module.exports = router;
 // admin url & 미들웨어
